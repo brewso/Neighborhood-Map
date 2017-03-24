24066651aa9559995cb864234ac2b47b7740ac05
brewso <ebrousseau23@gmail.com>
2017-03-23 21:03:13 -0500

    redone

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
-Neighborhood Map Project BY: Eric Brousseau
+Neighborhood Map Project BY: *Eric Brousseau*
 
-Click here!
-https://brewso.github.io/Neighborhood-Map
+[Click here!](https://brewso.github.io/Neighborhood-Map)
 OR
-UnPack files and Run index.html in your favorite web browser!
+UnPack files and Run **index.html** in your favorite web browser!
 
 
 This app is designed to show my favorite key points around the Twin cities metro area.
 There is a search function in a drop down list format that allows the user to search for a specific location out of the key points.
 Clicking on either the list item or the map marker will center the map, zoom to that location, and pop up an info window.
 The info window includes a google street view or better picture of that location along with some info on the specific location.
+There is also a button that allows the user to view a random set of *Flickr* images!
 
-In the upper left hand of the list is a box to display Accuweather current conditions, accuweather icon for the condition, and temp.
-
-The app is designed to be responsive to screen size and remove the list functionality when the screen is too cluttered.
+In the upper left hand of the list is a box to display *Accuweather* current conditions, accuweather icon for the condition, and temp.
 
 Location data provided by Google.
 
-
 Resources:
 
-Udacity GOOGLE MAPS APIS
-http://knockoutjs.com/documentation/html-binding.html
-https://developers.google.com
-https://apidev.accuweather.com/developers/
-http://www.w3schools.com/
+* Udacity GOOGLE MAPS APIS
+* http://knockoutjs.com/documentation/html-binding.html
+* https://developers.google.com
+* https://apidev.accuweather.com/developers/
+* http://www.w3schools.com/
--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
 'use strict';
-window.onload = loadMap;
 
 ///////////key variables for app to function///////////
-var center = {lat: 44.9552873,lng: -93.3176149}
-var accuweather = "g6xbpzaUcD1tGUMICwbwZcHSqzQQ4zp1";
-var googleID = "AIzaSyCOQXx5vDbUdJwzFRGhOdaE9vYFIKxpJPs";
-var map;
-var loaded = 0;
+var center = {lat: 44.9552873,lng: -93.3176149},
+    accuweather = "g6xbpzaUcD1tGUMICwbwZcHSqzQQ4zp1",
+    googleID = "AIzaSyCOQXx5vDbUdJwzFRGhOdaE9vYFIKxpJPs",
+    map,
+    loaded = 0;
+
 /////////set locations///////////
 var locations = [
   {name: "The Mall of America", address: "60 E Broadway, Bloomington, MN 55425", website: "www.mallofamerica.com", position: {lat: 44.8548651, lng: -93.2444035}},
   {name: "Minnehaha Falls", address: "4801 S Minnehaha Park, Minneapolis, MN 55417", website: "www.minneapolisparks.org", position: {lat: 44.9154592, lng: -93.2092766}},
-  {name: "The Sculpture Garden", address: "725 Vineland Pl, Minneapolis, MN 55403", website: "www.walkerart.org", position: {lat: 44.9699808, lng: -93.2891868}},
-  {name: "U.S. Bank Stadium", address: "401 Chicago Ave, Minneapolis, MN 55415", website: "www.usbankstadium.com", position: {lat: 44.971863, lng: -93.2561852}},
-  {name: "First Avenue", address: "701 N 1st Ave, Minneapolis, MN 55403", website: "www.first-avenue.com", position: {lat: 44.978702, lng: -93.2760631}},
-  {name: "Fat Lorenzo's", address: "5600 Cedar Ave S, Minneapolis, MN 55417", website: "www.fatlorenzos.com", position: {lat: 44.9008211, lng: -93.2475324}},
+  {name: "Sculpture Garden", address: "725 Vineland Pl, Minneapolis, MN 55403", website: "www.walkerart.org", position: {lat: 44.9699808, lng: -93.2891868}},
+  {name: "US Bank Stadium", address: "401 Chicago Ave, Minneapolis, MN 55415", website: "www.usbankstadium.com", position: {lat: 44.971863, lng: -93.2561852}},
+  {name: "First Avenue Minneapolis", address: "701 N 1st Ave, Minneapolis, MN 55403", website: "www.first-avenue.com", position: {lat: 44.978702, lng: -93.2760631}},
+  {name: "Fat Lorenzos", address: "5600 Cedar Ave S, Minneapolis, MN 55417", website: "www.fatlorenzos.com", position: {lat: 44.9008211, lng: -93.2475324}},
   {name: "Paisley Park", address: "7801 Audubon Rd, Chanhassen, MN 55317", website: "www.officialpaisleypark.com", position: {lat: 44.8618172, lng: -93.5613845}}
 ];
+
 ///////custom style of map//////////
 var styles = [
   {"elementType":"geometry","stylers":[{"color":"#ebe3cd"}]},
@@ -54,62 +55,98 @@ var styles = [
   {"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#92998d"}]}
 ];
 
-///////allows googlemaps script to run from javascript file////////////
-function loadMap() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=' + googleID + '&callback=startMap';
-    document.body.appendChild(script);
-}
-//////viewModel function that runs through KO to set markers and allow the list to function when clicked////////////
-var viewModel = function() {
-    var self = this;
+////handles making the markers and properties of the markers////
+var Pin = function(map, icon, i, name, address, website, position) {
 
-    self.markers = ko.observableArray();
+  this.title = name;
+  this.address = address;
+  this.url = website;
+  this.position = position;
+
+  var marker = new google.maps.Marker({
+     map: map,
+     title: this.title,
+     address: this.address,
+     url: this.url,
+     position: this.position,
+     icon: icon,
+     animation: google.maps.Animation.DROP,
+     visible: false,
+     streetViewImage: determineImage(i)
+  });
+
+  this.isVisible = ko.observable(false);
+
+  this.isVisible.subscribe(function(currentState) {
+    if (currentState) {
+      marker.setVisible(true);
+    } else {
+      marker.setVisible(false);
+    }
+  });
+
+  marker.addListener('click', function() {
+    ///////////set flickr images for specific location!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      flickrLocation = encodeURIComponent(this.title.trim());
+      flickrLat = position.lat;
+      flickrLng = position.lng;
+      imagesAreSet = false;
+      viewModel.navInfo(marker);
+  });
+
+  this.navClick = function() {
+    ///////////set flickr images for specific location!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      flickrLocation = encodeURIComponent(this.title.trim());
+      flickrLat = position.lat;
+      flickrLng = position.lng;
+      imagesAreSet = false;
+		viewModel.navInfo(marker);
+	};
+}
+
+//////viewModel function that runs through KO to set markers and allow the list to function when clicked////////////
+function viewModel() {
+    var self = this;
     self.filter = ko.observable('');
-    var largeInfo = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
+    var largeInfo = new google.maps.InfoWindow();
     var icon = 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    var markers = ko.observableArray();
+
+    //////clicking on list or marker calls this function////////
+    viewModel.navInfo = function(data) {
+      populateInfoWindow(data, largeInfo, bounds, icon);
+    };
 
     //////sets new marker for each location////////
+    viewModel.placeMarkers = ko.computed(function() {
     for (var i = 0; i < locations.length; i++) {
-        var position = locations[i].position;
-        var title = locations[i].name;
-        var address = locations[i].address;
-        var url = locations[i].website;
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: title,
-            address: address,
-            url: url,
-            icon: icon,
-            animation: google.maps.Animation.DROP,
-            streetViewImage: determineImage(i)
-        });
-        self.markers.push(marker);
-        marker.addListener('click', function() {
-            self.navInfo(this);
-        });
-        //////clicking on list or marker calls this function//////
-        self.navInfo = function(value) {
-            populateInfoWindow(value, largeInfo, bounds, icon);
-        };
-
-        bounds.extend(marker.position);
+      markers()[i] = new Pin(map, icon, i, locations[i].name, locations[i].address, locations[i].website, locations[i].position);
+        bounds.extend(markers()[i].position);
     }
     map.fitBounds(bounds);
-    //////filters locations for search////
-    self.navLocation = ko.computed(function() {
+    google.maps.event.addDomListener(window, 'resize', function() {
+      map.fitBounds(bounds);
+    });
+  });
+
+    //////filters locations for search/////
+    viewModel.navLocation = ko.computed(function() {
         var filter = self.filter().toLowerCase();
         if (!filter) {
-            return self.markers();
+          ko.utils.arrayForEach(markers(), function(item) {
+            item.isVisible(true);
+          });
+          return markers();
         } else {
-            return ko.utils.arrayFilter(self.markers(), function(item) {
-                return item.title.toLowerCase().indexOf(filter) > -1;
-            });
+          return ko.utils.arrayFilter(markers(), function(item) {
+      			var result = item.title.toLowerCase().indexOf(filter) > -1;
+            item.isVisible(result);
+            largeInfo.close();
+      			return result;
+          });
         }
-    }, self);
+    });
 }
 
 function populateInfoWindow(mark, largeInfo, bounds, icon) {
@@ -121,22 +158,21 @@ function populateInfoWindow(mark, largeInfo, bounds, icon) {
     setTimeout(function() {
         mark.setAnimation(null);
         mark.setIcon(icon);
-    }, 1500);
+    }, 1400);
     ////////////////Make sure the marker property is cleared if the infowindow is closed//////////
     largeInfo.addListener('closeclick', function() {
         mark.setIcon(icon);
     });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
 
     largeInfo.setContent('<img src="' + mark.streetViewImage + '" width= "180" height = "90"><br> <div><strong>' +
         mark.title + '</strong><br><hr style="margin-bottom: 5px"><p>' +
-        mark.address + '<br></p><a class="web-links" href="http://' + mark.url +
-        '" target="_blank">' + mark.url + '</a></div><br><p style="font-size:10px">Information provided by Google</p>');
+        mark.address + '<br></p><a class="web-links" href="http://' + mark.url + '" target="_blank">' +
+        mark.url + '</a></div><br><button onclick="infoClick()">See Flickr Photos</button><br>' +
+        '<p style="font-size:10px">Information provided by Google</p>');
     ////////////Open the infowindow on the correct marker///////////////////
     largeInfo.open(map, mark);
-
 }
+
 ///////////uses the position of the locations to get streetview for the mallofamerica and Minnehaha-Falls a set image is used//////////////
 function determineImage(i) {
     var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=180x90&location=';
@@ -163,8 +199,107 @@ function startMap() {
 
     ko.applyBindings(new viewModel());
 }
+
+////////////////////////////////////Flickr/////////////////////////////////////////////
+var flickrJSON,
+    flickrPhotoArray = [],
+    flickrLocation,
+    flickrLat,
+    flickrLng,
+    counter = 0,
+    imagesAreSet = false;
+
+//Binds the button in the infowindow to open flickr
+function infoClick(){
+  $(".flickr").css("z-index", "3");
+  $(".flickr").show();
+  setFlickrImages();
+}
+
+//Binds the x button to close flickr
+$("#exit-flickr").click(function() {
+    $(".flickr").hide();
+    imagesAreSet = true;
+});
+
+$("#right-arrow").click(forward);
+$("#left-arrow").click(backWard);
+
+//GET JSON from flickr
+//Display message if error getting flickr JSON
+function getFlickrImages() {
+		var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=23a0c4ed44c29e78558d11f9942c9b7f&text='+flickrLocation+'&accuracy=5&lat='+flickrLat+'&lon='+flickrLng+'&format=json';
+        $.ajax({
+            url: flickrUrl,
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            success: function(data) {
+                var photo = data.photos.photo;
+                flickrJSON = photo;
+            },
+            error: function() {
+				$('.flickr-image-container').append('<h1 style="text-align: center;">Sorry!</h1><br><h2 style="text-align: center;">Flickr Images Could Not Be Loaded</h2>');
+				$("#right-arrow").hide();
+				$("#left-arrow").hide();
+
+				}
+        });
+}
+
+//Get random images from flickr JSON
+//Store image data in flickrPhotoArray
+//Hide all images except the first
+function setFlickrImages() {
+	if(imagesAreSet === false) {
+    removeFlickrImages();
+    getFlickrImages();
+		setTimeout(function() {for(var i=0; i < 10; i++) {
+			var number = Math.floor((Math.random() * 45) + 1);
+			var photo = 'https://farm' + flickrJSON[number].farm + '.staticflickr.com/' + flickrJSON[number].server + '/' + flickrJSON[number].id + '_' + flickrJSON[number].secret + '.jpg';
+			flickrPhotoArray.push(photo);
+			$('.flickr-images').append('<img id="flickr-image' + i + '" src="' + photo + '" alt="' + flickrJSON[i].title + ' Flickr Image">');
+			$("#flickr-image" + i).hide();
+      imagesAreSet = true;
+			if(i < 1) {
+				$("#flickr-image" + i).show();
+		}
+	}}, 1500);
+	} else {
+		$("#flickr-image" + counter).show();
+	}
+}
+
+function removeFlickrImages(){
+for(var i=0; i < 10; i++) {
+  $("#flickr-image" + i).remove();
+}
+}
+
+//Bind click handler to arrow button to view next image
+function forward() {
+	$('#flickr-image' + counter).hide();
+	counter += 1;
+	if(counter >= 10) {
+		counter = 0;
+	}
+	$('#flickr-image' + counter).fadeIn(300);
+}
+
+//Bind click handler to arrow button to view previous image
+function backWard() {
+	$('#flickr-image' + counter).hide();
+	counter -= 1;
+	if(counter < 0) {
+		counter = 10;
+	}
+	$('#flickr-image' + counter).fadeIn(300);
+}
+
+////////***************************weather api************************************************///////////////
 var accuwUrl = "http://dataservice.accuweather.com/currentconditions/v1/348794.json?language=en&apikey=" + accuweather;
 var accuIcon = "https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/";
+var weatherTop = ko.observable('');
+var weatherBottom = ko.observable('');
 /////////////////Gets current conditions for the location.////////////
 $.ajax({
     type: "GET",
@@ -186,11 +321,13 @@ $.ajax({
             } else {
                 iconNum = conditions.WeatherIcon + '-s.png'
             }
-            list.append('<li><img style="width: 25px" src="' + accuIcon + iconNum + '">' + conditions.WeatherText + '</li>');
-            list.append('<li>Temp: ' + temp.Value + '° F</li>');
-        } else {
-            /////////////Error handling of accuweather///////////
-            list.append('<li> Sorry, Accuweather could not load!</li>');
-        }
+            weatherTop('<img style="width: 25px" src="' + accuIcon + iconNum + '">' + conditions.WeatherText);
+            weatherBottom('Temp: ' + temp.Value + '° F');
+            }
+        },
+        /////////////Error handling of accuweather///////////
+    error: function() {
+      weatherTop('Sorry, Accuweather could not load!');
+      weatherBottom('Reload app to try again!')
     }
 })
